e47a27d1edb14890541f6c9c577514b49742b8f4
Abhinav Venkat Sivakumar Gayathiri <Asivakumargayat1253@conestogac.on.ca>
2025-10-02 10:16:22 -0400

    adding debugger config

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -28,12 +28,12 @@ return {
     -- =====================
     -- ⌨️ Keybindings
     -- =====================
-    vim.keymap.set("n", "<F5>", dap.continue)
-    vim.keymap.set("n", "<F10>", dap.step_over)
-    vim.keymap.set("n", "<F11>", dap.step_into)
-    vim.keymap.set("n", "<F12>", dap.step_out)
-    vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
-    vim.keymap.set("n", "<Leader>B", function()
+    vim.keymap.set("n", "<leader>dc", dap.continue)
+    vim.keymap.set("n", "<leader>ds", dap.step_over)
+    vim.keymap.set("n", "<leader>di", dap.step_into)
+    vim.keymap.set("n", "<leader>do", dap.step_out)
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<Leader>dB", function()
       dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
     end)
     vim.keymap.set("n", "<Leader>lp", function()
